924fe8b889dda441b83b2fe556a042ce8c7c0f64
bautistaamarillo <amarillobautista5@gmail.com>
2023-04-11 18:52:26 -0300

    Reactividad en 2 sentidos

--- proyectoVueTres/js/main.js
+++ proyectoVueTres/js/main.js
@@ -3,12 +3,12 @@ const API = "https://api.github.com/users/";
 const app = Vue.createApp({
     data() {
         return {
-            nessage: "Hello Vue!"
+            search : null,
         };
     },
     methods: {
         async doSearch() {
-            const response = await fetch(API + 'bautistaamarillo')
+            const response = await fetch(API + this.search)
             const data = await response.json()
             console.log(data)
         }
